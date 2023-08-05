68e64cefa7a2ae853ac426d911dc0e4d0491d56b
muhammed-shafi007 <muahammedshafiali342@gmail.com>
2023-08-05 12:07:58 +0530

    trail app

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,35 @@
+class GroceryData{
+
+
+ static List<Map<String, dynamic>> groceryProducts = [
+  {
+    'id': '1',
+    'name': 'FASTRACK',
+    'description': '',
+    'price': 5.47,
+    'imageUrl': 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    'id': '2',
+    'name': 'SONY',
+    'description': '',
+    'price': 5.39,
+    'imageUrl': 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    'id': '3',
+    'name': 'Ray-Ban',
+    'description': '',
+    'price': 5.50,
+    'imageUrl': 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    'id': '4',
+    'name': 'NIKE',
+    'description': '',
+    'price': 4.89,
+    'imageUrl': 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+  },
+ 
+];
+}
